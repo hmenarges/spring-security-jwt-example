9ccdacefc276042f8e795aac1305417bf95d8dfb
Narges Hashemi <hasheminarges76@gmail.com>
2025-08-24 14:05:33 +0330

    update JWT dependencies in pom.xml and refactor extractAllClaims with approperiate method

--- src/main/java/com/javatechie/jwt/api/util/JwtUtil.java
+++ src/main/java/com/javatechie/jwt/api/util/JwtUtil.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 @Service
@@ -29,7 +30,7 @@ public class JwtUtil {
         return claimsResolver.apply(claims);
     }
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(secret).build().parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token) {
@@ -44,7 +45,7 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(10)))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
     }
 
